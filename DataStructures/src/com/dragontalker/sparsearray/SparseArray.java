8547dd126069337c3ccee94a7dfcd32154f3f50b
Richard <richard.yang.tong@gmail.com>
2021-07-28 12:10:07 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/sparsearray/SparseArray.java
+++ DataStructures/src/com/dragontalker/sparsearray/SparseArray.java
@@ -1,7 +1,33 @@
 package com.dragontalker.sparsearray;
 
 public class SparseArray {
+
     public static void main(String[] args) {
-        
+        // 创建一个原始的二维数组 11*11
+        // 0: 表示没有棋子, 1: 表示黑子, 2: 表示蓝子
+        int chessArr1[][] = new int[11][11];
+        chessArr1[1][2] = 1;
+        chessArr1[2][3] = 2;
+
+        // 输出原始的二维数组
+        System.out.println("原始的二维数组");
+        for (int[] row : chessArr1) {
+            for (int data : row ) {
+                System.out.printf("%d\t", data);
+            }
+            System.out.println();
+        }
+
+        // 将二维数组 转 稀疏数组
+        // 1. 先遍历二维数组 得到非0数据的个数
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr1[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        System.out.println("sum=" + sum);
     }
 }
