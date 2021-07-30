377e5802a76b94cd78a4e86f31ae239814f6055c
Richard <richard.yang.tong@gmail.com>
2021-07-30 16:34:22 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/SingleLinkedListDemo.java
+++ DataStructures/src/com/dragontalker/linkedlist/SingleLinkedListDemo.java
@@ -9,7 +9,27 @@ public class SingleLinkedListDemo {
 
 // 定义SingleLinkedList
 class SingleLinkedList {
+    // 先初始化一个头节点, 头节点不要动, 不存放具体的数据
+    private HeroNode head = new HeroNode(0, null, null);
 
+    // 添加节点到单向链表
+    // 思路: 当不考虑变好顺序时
+    // 1. 找到当前链表的最后节点
+    // 2. 将最后这个节点的next 指向 新的节点
+    public void add(HeroNode heroNode) {
+
+        // 因为head节点不能动, 因此我们需要一个辅助变量 temp
+        HeroNode temp = head;
+        // 遍历链表, 找到最后
+        while(true) {
+            // 找到链表的最后
+            if (temp.next == null) {
+                temp.next = heroNode;
+                break;
+            }
+            temp = temp.next;
+        }
+    }
 }
 
 // 定义HeroNode, 每个HeroNode 对象就是一个节点
