aa17502b77d08fd534ee6f1150d1c1da3ff5681a
Richard <richard.yang.tong@gmail.com>
2021-07-30 16:41:53 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/SingleLinkedListDemo.java
+++ DataStructures/src/com/dragontalker/linkedlist/SingleLinkedListDemo.java
@@ -3,6 +3,13 @@ package com.dragontalker.linkedlist;
 public class SingleLinkedListDemo {
 
     public static void main(String[] args) {
+        // 进行测试
+        // 先创建节点
+        HeroNode hero1 = new HeroNode(1, "宋江", "及时雨");
+        HeroNode hero2 = new HeroNode(2, "卢俊义", "玉麒麟");
+        HeroNode hero3 = new HeroNode(3, "吴用", "智多星");
+        HeroNode hero4 = new HeroNode(4, "林冲", "豹子头");
+
 
     }
 }
