c3644011443a3a065c81d506eaad4c85d6cc4c64
Richard <richard.yang.tong@gmail.com>
2021-07-30 11:05:11 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/queue/ArrayQueueDemo.java
+++ DataStructures/src/com/dragontalker/queue/ArrayQueueDemo.java
@@ -1,4 +1,13 @@
 package com.dragontalker.queue;
 
 public class ArrayQueueDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+// 使用数组模拟队列 - 编写一个ArrayQueue类
+class ArrayQueue {
+
 }
