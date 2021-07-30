b615b197410e534635f9366ac3a95808fd131897
Richard <richard.yang.tong@gmail.com>
2021-07-30 12:24:15 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/queue/CircleArrayQueueDemo.java
+++ DataStructures/src/com/dragontalker/queue/CircleArrayQueueDemo.java
@@ -6,3 +6,10 @@ public class CircleArrayQueueDemo {
 
     }
 }
+
+class CircleArray {
+    private int maxSize; // 表示数组的最大容量
+    private int front; // 队列头
+    private int rear; // 队列尾
+    private int[] arr; // 该数组用于存放数据, 模拟队列
+}
