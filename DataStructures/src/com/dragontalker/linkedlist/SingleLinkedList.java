dc4eb0a497071c7ba443a2e92ed1edaf7df34c27
Richard <richard.yang.tong@gmail.com>
2021-07-30 16:24:23 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/SingleLinkedList.java
+++ DataStructures/src/com/dragontalker/linkedlist/SingleLinkedList.java
@@ -13,4 +13,11 @@ class HeroNode {
     public String name;
     public String nickname;
     public HeroNode next;
+
+    // 构造器
+    public HeroNode(int no, String name, String nickname) {
+        this.no = no;
+        this.name = name;
+        this.nickname = nickname;
+    }
 }
