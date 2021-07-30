2ff3145463e6d61640020cbeb3a7a08321b433e6
Richard <richard.yang.tong@gmail.com>
2021-07-30 11:52:49 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/queue/ArrayQueueDemo.java
+++ DataStructures/src/com/dragontalker/queue/ArrayQueueDemo.java
@@ -7,7 +7,7 @@ public class ArrayQueueDemo {
     public static void main(String[] args) {
         // 测试一把
         // 创建与一个队列
-        ArrayQueue arrayQueue = new ArrayQueue(3);
+        ArrayQueue queue = new ArrayQueue(3);
         char key = ' '; // 接收用户输入
 
         Scanner scanner = new Scanner(System.in);
@@ -20,6 +20,19 @@ public class ArrayQueueDemo {
             System.out.println("a(add): 添加数据到队列");
             System.out.println("g(get): 从队列取出数据");
             System.out.println("h(head): 查看队列头的数据");
+            key = scanner.next().charAt(0); // 接收一个字符
+
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+            }
+
         }
     }
 }
