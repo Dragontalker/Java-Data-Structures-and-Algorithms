def5f749458eda909839ec2220afc6d3018bfdca
Richard <richard.yang.tong@gmail.com>
2021-07-30 11:06:27 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/queue/ArrayQueueDemo.java
+++ DataStructures/src/com/dragontalker/queue/ArrayQueueDemo.java
@@ -9,5 +9,9 @@ public class ArrayQueueDemo {
 
 // 使用数组模拟队列 - 编写一个ArrayQueue类
 class ArrayQueue {
+    private int maxSize; // 表示数组的最大容量
+    private int front; // 队列头
+    private int rear; // 队列尾
+    private int[] arr; // 该数组用于存放数据, 模拟队列
 
 }
