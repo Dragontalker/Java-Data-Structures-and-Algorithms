a3e5a604dd4ffe810c463d807675765e22882ff9
Richard <richard.yang.tong@gmail.com>
2021-07-30 15:42:06 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/queue/CircleArrayQueueDemo.java
+++ DataStructures/src/com/dragontalker/queue/CircleArrayQueueDemo.java
@@ -1,9 +1,61 @@
 package com.dragontalker.queue;
 
+import java.util.Scanner;
+
 public class CircleArrayQueueDemo {
 
     public static void main(String[] args) {
+        // 测试一把
+        System.out.println("测试数组模拟环形队列的案例");
+        // 创建与一个队列
+        ArrayQueue queue = new ArrayQueue(3);
+        char key = ' '; // 接收用户输入
 
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+
+        // 输入一个菜单
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("g(get): 从队列取出数据");
+            System.out.println("h(head): 查看队列头的数据");
+            key = scanner.next().charAt(0); // 接收一个字符
+
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g': // 取出数据
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h': // 查看队列头的数据
+                    try {
+                        int res = queue.headQueuePeak();
+                        System.out.printf("队列头的数据是%d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e': // 退出
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
 
