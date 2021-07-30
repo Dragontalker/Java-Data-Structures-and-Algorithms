07786b14aa525205026f241a7902610501aa0d64
Richard <richard.yang.tong@gmail.com>
2021-07-30 12:26:39 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/queue/CircleArrayQueueDemo.java
+++ DataStructures/src/com/dragontalker/queue/CircleArrayQueueDemo.java
@@ -9,9 +9,11 @@ public class CircleArrayQueueDemo {
 
 class CircleArray {
     private int maxSize; // 表示数组的最大容量
-    // front 变量的含义做一个调整: front就指向队列的第一个元素, 也就是说 arr[front]
-    // front 的初始值为0
+    // front 变量的含义做一个调整: front 就指向队列的第一个元素, 也就是说 arr[front]
+    // front 的初始值 = 0
     private int front; // 队列头
+    // rear 变量的含义做一个调整: rear 指向队列的最后一个元素的后一个位置, 因为希望空出一个位置
+    // rear 的初始值 = 0
     private int rear; // 队列尾
     private int[] arr; // 该数组用于存放数据, 模拟队列
 }
