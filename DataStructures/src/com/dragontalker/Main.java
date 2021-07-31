222f5fbba364c286a490be302b604dc215d2f43e
Richard <richard.yang.tong@gmail.com>
2021-07-31 13:49:02 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/Main.java
+++ DataStructures/src/com/dragontalker/Main.java
@@ -3,6 +3,6 @@ package com.dragontalker;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Java - Data Structures and Algorithms");
     }
 }
