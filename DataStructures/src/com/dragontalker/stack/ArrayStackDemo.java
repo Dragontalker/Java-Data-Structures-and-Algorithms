3f86a08154ee69e4e08efafff97066ed84fa3b3a
Richard <richard.yang.tong@gmail.com>
2021-09-19 18:05:09 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/stack/ArrayStackDemo.java
+++ DataStructures/src/com/dragontalker/stack/ArrayStackDemo.java
@@ -1,11 +1,50 @@
 package com.dragontalker.stack;
 
-import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class ArrayStackDemo {
 
     public static void main(String[] args) {
+        // 测试一下ArrayStack是否正确
+        // 先创建一个ArrayStack的对象
+        ArrayStack stack = new ArrayStack(4);
+        String key;
+        boolean loop = true; // 控制是否退出菜单
+        Scanner scanner = new Scanner(System.in);
 
+        while(loop) {
+            System.out.println("show: 表示显示栈");
+            System.out.println("exit: 退出程序");
+            System.out.println("push: 表示添加数据到栈");
+            System.out.println("pop: 表示从栈取出数据");
+            System.out.println("请输入你的选择");
+            key = scanner.next();
+            switch (key) {
+                case "show":
+                    stack.list();
+                    break;
+                case "push":
+                    System.out.println("请输入一个数: ");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+                case "pop":
+                    try {
+                        int res = stack.pop();
+                        System.out.printf("出栈的数据时 %d\n", res);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "exit":
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+        System.out.println("程序退出了");
     }
 }
 
