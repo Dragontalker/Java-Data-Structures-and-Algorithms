6d57cd61999eac33d7d1917b8656bc8f12b48878
Richard <richard.yang.tong@gmail.com>
2021-09-19 13:47:38 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/DoubleLinkedListDemo.java
+++ DataStructures/src/com/dragontalker/linkedlist/DoubleLinkedListDemo.java
@@ -1,4 +1,13 @@
 package com.dragontalker.linkedlist;
 
 public class DoubleLinkedListDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+// 创建一个双向链表的类
+class DoubleLinkedList {
+
 }
