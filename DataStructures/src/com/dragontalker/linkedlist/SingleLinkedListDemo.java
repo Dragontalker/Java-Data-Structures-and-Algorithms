cf5c95f36367290a750c6c3990bb532d5223a274
Richard <richard.yang.tong@gmail.com>
2021-09-18 21:09:06 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/SingleLinkedListDemo.java
+++ DataStructures/src/com/dragontalker/linkedlist/SingleLinkedListDemo.java
@@ -49,6 +49,29 @@ public class SingleLinkedListDemo {
         System.out.println("res = " + res);
     }
 
+    // 将单链表反转
+    public static void reverseList(HeroNode head) {
+        // 如果当前链表为空, 或者只有一个节点, 就无需反转, 直接返回
+        if(head.next == null || head.next.next == null) {
+            return;
+        }
+
+        // 定义一个辅助的指针(变量), 帮助我们遍历原来的链表
+        HeroNode cur = head.next;
+        HeroNode next = null; // 指向当前节点[cur]的下一个节点
+        HeroNode reverseHead = new HeroNode(0, "", "");
+        // 遍历原来的链表
+        // 每遍历一个节点, 就将其取出
+        // 并放在新的链表 reverseHead 的最前端
+        while(cur != null) {
+            next = cur.next; // 先暂时保存当前节点的下一个节点, 因为后面需要使用
+            cur.next = reverseHead.next; // 将cur的下一个节点指向新的链表的最前端
+            cur = next; // 让cur后移
+        }
+        // 将head.next 指向 reverseHead.next , 实现单链表的反装
+        head.next = reverseHead.next;
+    }
+
     // 查找单链表中的倒数第k个节点 [新浪面试题]
     // 思路
     // 1. 编写一个方法, 接收head节点, 同时接收一个index
