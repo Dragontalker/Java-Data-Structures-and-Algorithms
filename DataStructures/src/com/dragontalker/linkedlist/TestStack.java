64204e6835e94392591bf9fff606eacdb73df725
Richard <richard.yang.tong@gmail.com>
2021-09-18 21:29:35 -0400

    Added notes.

---
+++ DataStructures/src/com/dragontalker/linkedlist/TestStack.java
@@ -0,0 +1,21 @@
+package com.dragontalker.linkedlist;
+
+import java.util.Stack;
+
+// 演示栈Stack的基本使用
+public class TestStack {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        // 入栈
+        stack.add("Jack");
+        stack.add("Tom");
+        stack.add("Smith");
+
+        // 出栈
+        // Smith, Tom, Jack
+        while(stack.size() > 0) {
+            System.out.println(stack.pop()); // pop就是将栈顶的数据取出
+        }
+    }
+}
