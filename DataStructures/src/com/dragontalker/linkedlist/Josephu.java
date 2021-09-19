206584c8bd5fd992dc96521831848caafb071479
Richard <richard.yang.tong@gmail.com>
2021-09-19 16:23:08 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/Josephu.java
+++ DataStructures/src/com/dragontalker/linkedlist/Josephu.java
@@ -7,6 +7,36 @@ public class Josephu {
     }
 }
 
+// 创建一个环形的单向列表
+class CircleSingleLinkedList {
+    // 创建一个first节点, 当前没有编号
+    private Boy first = new Boy(-1);
+    // 添加小孩节点, 构建成一个环形的链表
+    public void addBoy(int nums) {
+        // nums 做一个数据校验
+        if(nums < 1) {
+            System.out.println("nums的值不正确");
+            return;
+        }
+        Boy curBoy = null; // 辅助指针, 帮助构建环形链表
+        // 使用for循环来创建我们的环形链表
+        for(int i = 1; i <= nums; i++) {
+            // 根据编号创建小孩节点
+            Boy boy = new Boy(i);
+            // 如果是第一个小孩
+            if (i == 1) {
+                first = boy;
+                first.setNext(first); // 构成环
+                curBoy = first;
+            } else {
+                curBoy.setNext(boy);
+                boy.setNext(first);
+                curBoy = boy;
+            }
+        }
+    }
+}
+
 // 创建一个Boy类, 表示一个节点
 class Boy {
     private int no; // 编号
