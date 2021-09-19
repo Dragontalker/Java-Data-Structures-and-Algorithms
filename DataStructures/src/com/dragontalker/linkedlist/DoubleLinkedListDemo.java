b8f6ef7be44dca7ff75e09739b04b42b499153b1
Richard <richard.yang.tong@gmail.com>
2021-09-19 13:49:04 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/linkedlist/DoubleLinkedListDemo.java
+++ DataStructures/src/com/dragontalker/linkedlist/DoubleLinkedListDemo.java
@@ -11,3 +11,30 @@ public class DoubleLinkedListDemo {
 class DoubleLinkedList {
 
 }
+
+// 定义 HeroNode2 , 每个HeroNode 对象就是一个节点
+class HeroNode2 {
+    public int no;
+    public String name;
+    public String nickname;
+    public HeroNode next; // 指向下一个节点, 默认为null
+    public HeroNode pre; // 指向下一个节点, 默认为null
+
+    // 构造器
+    public HeroNode2(int no, String name, String nickname) {
+        this.no = no;
+        this.name = name;
+        this.nickname = nickname;
+    }
+
+    // 为了显示方便, 我们重写toString
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                ", nickname='" + nickname + '\'' +
+                '}';
+    }
+}
