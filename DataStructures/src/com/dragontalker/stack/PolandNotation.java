fda30223c90f4089d19fa5a08cb4504524ee7e03
Richard <richard.yang.tong@gmail.com>
2021-09-20 11:51:46 -0400

    Added notes.

--- DataStructures/src/com/dragontalker/stack/PolandNotation.java
+++ DataStructures/src/com/dragontalker/stack/PolandNotation.java
@@ -11,12 +11,17 @@ public class PolandNotation {
         // (3+4)x5-6 => 3 4 + 5 x 6 -
         // 为了说明方便, 逆波兰表达式的数字和符号使用空格隔开
         String suffixExpression = "3 4 + 5 x 6 -";
+        System.out.println("逆波兰表达式=" + suffixExpression);
         // 思路
         // 1. 先将 "3 4 + 5 x 6 -" => 放到ArrayList中
         // 2. 将 ArrayList 传递给一个方法, 遍历 ArrayList 配合栈 完成计算
 
-        List<String> rpnList = getListString(suffixExpression);
-        System.out.println("rpnList=" + rpnList);
+        List<String> list = getListString(suffixExpression);
+        System.out.println("rpnList=" + list);
+
+        int res = calculate(list);
+        System.out.println("计算的结果是=" + res);
+
     }
 
     // 将一个逆波兰表达式, 依次将数据和运算符 放入到 ArrayList中
